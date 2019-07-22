bb347816a68f407a7ae06e34eaa00f050bac4646
zhengp0 <zhengp@uw.edu>
2019-07-22 11:37:52 -0700

    package: advance to version 0.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='xspline',
-      version='0.0.0',
+      version='0.0.1',
       description='robust spline package',
       url='https://github.com/zhengp0/bspline',
       author='Peng Zheng',
