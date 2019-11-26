14914d3284123c40539cc99526aecc254de67ad1
zhengp0 <zhengp@uw.edu>
2019-11-26 13:25:17 -0800

    modify: change name from bspline_b to bspline_domain and f to fun for readability.

--- src/xspline/core.py
+++ src/xspline/core.py
@@ -9,7 +9,7 @@ import numpy as np
 from . import utils
 
 
-def bspline_b(knots, degree, idx, l_extra=False, r_extra=False):
+def bspline_domain(knots, degree, idx, l_extra=False, r_extra=False):
     r"""Compute the support for the spline basis, knots degree and the index of
     the basis.
 
@@ -52,48 +52,48 @@ def bspline_b(knots, degree, idx, l_extra=False, r_extra=False):
     return np.array([lb, ub])
 
 
-def bspline_f(x, knots, degree, idx, l_extra=False, r_extra=False):
+def bspline_fun(x, knots, degree, idx, l_extra=False, r_extra=False):
     num_knots = knots.size
     num_intervals = num_knots - 1
     num_splines = num_intervals + degree
 
-    b = bspline_b(knots, degree, idx, l_extra=l_extra, r_extra=r_extra)
+    b = bspline_domain(knots, degree, idx, l_extra=l_extra, r_extra=r_extra)
 
     if degree == 0:
         f = utils.indicator_f(x, b, r_close=(idx == num_splines - 1))
         return f
 
     if idx == 0:
-        b_effect = bspline_b(knots, degree, idx)
+        b_effect = bspline_domain(knots, degree, idx)
         y = utils.indicator_f(x, b)
         z = utils.linear_rf(x, b_effect)
         return y*(z**degree)
 
     if idx == num_splines - 1:
-        b_effect = bspline_b(knots, degree, idx)
+        b_effect = bspline_domain(knots, degree, idx)
         y = utils.indicator_f(x, b, r_close=True)
         z = utils.linear_lf(x, b_effect)
         return y*(z**degree)
 
-    lf = bspline_f(x, knots, degree - 1, idx - 1,
-                   l_extra=l_extra, r_extra=r_extra)
-    lf *= utils.linear_lf(x, bspline_b(knots, degree - 1, idx - 1))
+    lf = bspline_fun(x, knots, degree - 1, idx - 1,
+                     l_extra=l_extra, r_extra=r_extra)
+    lf *= utils.linear_lf(x, bspline_domain(knots, degree - 1, idx - 1))
 
-    rf = bspline_f(x, knots, degree - 1, idx,
-                   l_extra=l_extra, r_extra=r_extra)
-    rf *= utils.linear_rf(x, bspline_b(knots, degree - 1, idx))
+    rf = bspline_fun(x, knots, degree - 1, idx,
+                     l_extra=l_extra, r_extra=r_extra)
+    rf *= utils.linear_rf(x, bspline_domain(knots, degree - 1, idx))
 
     return lf + rf
 
 
-def bspline_df(x, knots, degree, order, idx, l_extra=False, r_extra=False):
+def bspline_dfun(x, knots, degree, order, idx, l_extra=False, r_extra=False):
     num_knots = knots.size
     num_intervals = num_knots - 1
     num_splines = num_intervals + degree
 
     if order == 0:
-        return bspline_f(x, knots, degree, idx,
-                         l_extra=l_extra, r_extra=r_extra)
+        return bspline_fun(x, knots, degree, idx,
+                           l_extra=l_extra, r_extra=r_extra)
 
     if order > degree:
         return np.zeros(x.size)
@@ -101,61 +101,61 @@ def bspline_df(x, knots, degree, order, idx, l_extra=False, r_extra=False):
     if idx == 0:
         rdf = 0.0
     else:
-        b = bspline_b(knots, degree - 1, idx - 1)
+        b = bspline_domain(knots, degree - 1, idx - 1)
         d = b[1] - b[0]
         f = (x - b[0])/d
-        rdf = f*bspline_df(x, knots, degree - 1, order, idx - 1,
-                           l_extra=l_extra, r_extra=r_extra)
-        rdf += order*bspline_df(x, knots, degree - 1, order - 1, idx - 1,
-                                l_extra=l_extra, r_extra=r_extra)/d
+        rdf = f*bspline_dfun(x, knots, degree - 1, order, idx - 1,
+                             l_extra=l_extra, r_extra=r_extra)
+        rdf += order*bspline_dfun(x, knots, degree - 1, order - 1, idx - 1,
+                                  l_extra=l_extra, r_extra=r_extra)/d
 
     if idx == num_splines - 1:
         ldf = 0.0
     else:
-        b = bspline_b(knots, degree - 1, idx)
+        b = bspline_domain(knots, degree - 1, idx)
         d = b[0] - b[1]
         f = (x - b[1])/d
-        ldf = f*bspline_df(x, knots, degree - 1, order, idx,
-                           l_extra=l_extra, r_extra=r_extra)
-        ldf += order*bspline_df(x, knots, degree - 1, order - 1, idx,
-                                l_extra=l_extra, r_extra=r_extra)/d
+        ldf = f*bspline_dfun(x, knots, degree - 1, order, idx,
+                             l_extra=l_extra, r_extra=r_extra)
+        ldf += order*bspline_dfun(x, knots, degree - 1, order - 1, idx,
+                                  l_extra=l_extra, r_extra=r_extra)/d
 
     return ldf + rdf
 
 
-def bspline_if(a, x, knots, degree, order, idx, l_extra=False, r_extra=False):
+def bspline_ifun(a, x, knots, degree, order, idx, l_extra=False, r_extra=False):
     num_knots = knots.size
     num_intervals = num_knots - 1
     num_splines = num_intervals + degree
 
     if order == 0:
-        return bspline_f(x, knots, degree, idx,
-                         l_extra=l_extra, r_extra=r_extra)
+        return bspline_fun(x, knots, degree, idx,
+                           l_extra=l_extra, r_extra=r_extra)
 
     if degree == 0:
-        b = bspline_b(knots, degree, idx, l_extra=l_extra, r_extra=r_extra)
+        b = bspline_domain(knots, degree, idx, l_extra=l_extra, r_extra=r_extra)
         return utils.indicator_if(a, x, order, b)
 
     if idx == 0:
         rif = 0.0
     else:
-        b = bspline_b(knots, degree - 1, idx - 1)
+        b = bspline_domain(knots, degree - 1, idx - 1)
         d = b[1] - b[0]
         f = (x - b[0]) / d
-        rif = f*bspline_if(a, x, knots, degree - 1, order, idx - 1,
-                           l_extra=l_extra, r_extra=r_extra)
-        rif -= order*bspline_if(a, x, knots, degree - 1, order + 1, idx - 1,
-                                l_extra=l_extra, r_extra=r_extra)/d
+        rif = f*bspline_ifun(a, x, knots, degree - 1, order, idx - 1,
+                             l_extra=l_extra, r_extra=r_extra)
+        rif -= order*bspline_ifun(a, x, knots, degree - 1, order + 1, idx - 1,
+                                  l_extra=l_extra, r_extra=r_extra)/d
 
     if idx == num_splines - 1:
         lif = 0.0
     else:
-        b = bspline_b(knots, degree - 1, idx)
+        b = bspline_domain(knots, degree - 1, idx)
         d = b[0] - b[1]
         f = (x - b[1]) / d
-        lif = f*bspline_if(a, x, knots, degree - 1, order, idx,
-                           l_extra=l_extra, r_extra=r_extra)
-        lif -= order*bspline_if(a, x, knots, degree - 1, order + 1, idx,
-                                l_extra=l_extra, r_extra=r_extra)/d
+        lif = f*bspline_ifun(a, x, knots, degree - 1, order, idx,
+                             l_extra=l_extra, r_extra=r_extra)
+        lif -= order*bspline_ifun(a, x, knots, degree - 1, order + 1, idx,
+                                  l_extra=l_extra, r_extra=r_extra)/d
 
     return lif + rif
--- tests/test_core.py
+++ tests/test_core.py
@@ -16,26 +16,26 @@ from xspline import core
                           (0, 1, np.array([1.0, 2.0])),
                           (1, 0, np.array([0.0, 1.0])),
                           (1, 1, np.array([0.0, 2.0]))])
-def test_core_bspline_b(knots, degree, idx, result):
-    my_result = core.bspline_b(knots, degree, idx)
+def test_core_bspline_domain(knots, degree, idx, result):
+    my_result = core.bspline_domain(knots, degree, idx)
     assert np.linalg.norm(my_result - result) < 1e-10
 
 
 @pytest.mark.parametrize("knots", [np.arange(5).astype(float)])
 @pytest.mark.parametrize("degree", [0, 1])
-def test_core_bspline_b_l_extra(knots, degree):
+def test_core_bspline_domain_l_extra(knots, degree):
     idx = 0
     result = np.array([-np.inf, knots[1]])
-    my_result = core.bspline_b(knots, degree, idx, l_extra=True)
+    my_result = core.bspline_domain(knots, degree, idx, l_extra=True)
     assert np.isneginf(my_result[0])
     assert np.abs(my_result[1] - result[1]) < 1e-10
 
 
 @pytest.mark.parametrize("knots", [np.arange(5).astype(float)])
 @pytest.mark.parametrize("degree", [0, 1])
-def test_core_bspline_b_r_extra(knots, degree):
+def test_core_bspline_domain_r_extra(knots, degree):
     idx = knots.size + degree - 2
     result = np.array([knots[-2], np.inf])
-    my_result = core.bspline_b(knots, degree, idx, r_extra=True)
+    my_result = core.bspline_domain(knots, degree, idx, r_extra=True)
     assert np.isposinf(my_result[1])
     assert np.abs(my_result[0] - result[0]) < 1e-10
