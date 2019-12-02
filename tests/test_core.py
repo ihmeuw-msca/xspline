004ca93f3109cc5911ae776cecf2f3205823dad7
zhengp0 <zhengp@uw.edu>
2019-12-02 10:46:56 -0800

    update: add tests for

--- tests/test_core.py
+++ tests/test_core.py
@@ -55,3 +55,49 @@ def test_bspline_domain_r_extra(knots, degree):
     my_result = core.bspline_domain(knots, degree, idx, r_extra=True)
     assert np.isposinf(my_result[1])
     assert np.abs(my_result[0] - result[0]) < 1e-10
+
+
+@pytest.mark.parametrize("knots", [np.linspace(0.0, 1.0, 5)])
+@pytest.mark.parametrize("degree", [1])
+@pytest.mark.parametrize("idx", [0, 1])
+@pytest.mark.parametrize("x", [np.linspace(0.0, 1.0, 101)])
+def test_bspline_fun(knots, degree, idx, x):
+    my_y = core.bspline_fun(x, knots, degree, idx)
+    if idx == 0:
+        tr_y = np.maximum((knots[1] - x)/knots[1], 0.0)
+    else:
+        tr_y = np.zeros(x.size)
+        idx1 = (x >= knots[0]) & (x < knots[1])
+        idx2 = (x >= knots[1]) & (x < knots[2])
+        tr_y[idx1] = x[idx1]/knots[1]
+        tr_y[idx2] = (knots[2] - x[idx2])/(knots[2] - knots[1])
+
+    assert np.linalg.norm(tr_y - my_y) < 1e-10
+
+
+@pytest.mark.parametrize("knots", [np.linspace(0.0, 1.0, 5)])
+@pytest.mark.parametrize("x", [np.linspace(-1.0, -0.1, 10)])
+@pytest.mark.parametrize("l_extra", [True, False])
+def test_bspline_l_extra(x, knots, l_extra):
+    degree = 0
+    idx = 0
+    my_y = core.bspline_fun(x, knots, degree, idx, l_extra=l_extra)
+    if l_extra:
+        tr_y = 1.0
+    else:
+        tr_y = 0.0
+    assert np.linalg.norm(my_y - tr_y) < 1e-10
+
+
+@pytest.mark.parametrize("knots", [np.linspace(0.0, 1.0, 5)])
+@pytest.mark.parametrize("x", [np.linspace(1.1, 2.0, 10)])
+@pytest.mark.parametrize("r_extra", [True, False])
+def test_bspline_r_extra(x, knots, r_extra):
+    degree = 0
+    idx = -1
+    my_y = core.bspline_fun(x, knots, degree, idx, r_extra=r_extra)
+    if r_extra:
+        tr_y = 1.0
+    else:
+        tr_y = 0.0
+    assert np.linalg.norm(my_y - tr_y) < 1e-10
