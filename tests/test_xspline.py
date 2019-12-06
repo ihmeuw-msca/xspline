00d180dc14a60fbfcae0aadf2c29efe5004451ed
zhengp0 <zhengp@uw.edu>
2019-12-05 16:33:23 -0800

    update: add the tests for   function.

--- tests/test_xspline.py
+++ tests/test_xspline.py
@@ -17,7 +17,7 @@ def knots():
 
 @pytest.fixture
 def degree():
-    return 3
+    return 1
 
 
 @pytest.mark.parametrize("l_linear", [True, False])
@@ -31,11 +31,38 @@ def test_domain(knots, degree, idx, l_linear, r_linear, l_extra, r_extra):
     if idx == 0:
         lb = -np.inf if l_extra else xs.knots[0]
     else:
-        lb = xs.inner_knots[-2] if r_linear else xs.knots[-2]
+        lb = xs.inner_knots[-2]
     if idx == -1:
         ub = np.inf if r_extra else xs.knots[-1]
     else:
-        ub = xs.inner_knots[1] if l_linear else xs.knots[1]
+        ub = xs.inner_knots[1]
 
     assert my_domain[0] == lb
     assert my_domain[1] == ub
+
+
+@pytest.mark.parametrize("l_linear", [True, False])
+@pytest.mark.parametrize("r_linear", [True, False])
+@pytest.mark.parametrize("l_extra", [True, False])
+@pytest.mark.parametrize("r_extra", [True, False])
+@pytest.mark.parametrize("idx", [0, -1])
+def test_fun(knots, degree, idx, l_linear, r_linear, l_extra, r_extra):
+    xs = XSpline(knots, degree, l_linear=l_linear, r_linear=r_linear)
+    if idx == 0:
+        x = np.linspace(xs.knots[0] - 1.0, xs.knots[1], 101)
+    else:
+        x = np.linspace(xs.knots[-2], xs.knots[-1], 101)
+    my_y = xs.fun(x, idx, l_extra=l_extra, r_extra=r_extra)
+
+    if idx == 0:
+        tr_y = (xs.inner_knots[1] - x) / \
+               (xs.inner_knots[1] - xs.inner_knots[0])
+        if not l_extra:
+            tr_y[x < xs.knots[0]] = 0.0
+    else:
+        tr_y = (x - xs.inner_knots[-2]) / \
+               (xs.inner_knots[-1] - xs.inner_knots[-2])
+        if not r_extra:
+            tr_y[x > xs.knots[-1]] = 0.0
+
+    assert np.linalg.norm(my_y - tr_y) < 1e-10
