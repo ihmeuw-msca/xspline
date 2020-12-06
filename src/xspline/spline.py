12a774ba84090dd29c90d6c76d1b8dd9cf898cf5
zhengp0 <zhengp@uw.edu>
2020-12-06 15:54:23 -0800

    incorporate get_spline_bases in XSpline class

--- src/xspline/spline.py
+++ src/xspline/spline.py
@@ -5,35 +5,15 @@ from typing import Iterable
 
 import numpy as np
 
-from xspline.basis import SplineBasis, SplineSpecs
+from xspline.basis import SplineBasis, SplineSpecs, get_spline_bases
 
 
 class XSpline:
     def __init__(self,
                  knots: Iterable,
-                 degree: int,
-                 l_linx: bool = False,
-                 r_linx: bool = False):
+                 degree: int):
         self.specs = SplineSpecs(knots, degree)
-        self.l_linx = l_linx
-        self.r_linx = r_linx
-        self.bases = []
-        for d in range(self.specs.degree + 1):
-            specs = SplineSpecs(self.specs.knots, d, i)
-            self.bases.append([
-                SplineBasis(specs)
-                for i in range(specs.num_bases)
-            ])
-
-        for d in range(1, self.specs.degree + 1):
-            bases = self.bases[d]
-            bases_prev = self.bases[d - 1]
-            for i, basis in enumerate(bases):
-                indices = set([
-                    max(0, i - 1),
-                    min(i, len(bases_prev) - 1)
-                ])
-                basis.link_bases([bases_prev[j] for j in indices])
+        self.bases = get_spline_bases(self.specs)
 
     def design_mat(self, data: np.ndarray, order: int = 0) -> np.ndarray:
         data = np.asarray(data)
@@ -43,3 +23,6 @@ class XSpline:
             fun(data, order)[:, None]
             for fun in self.bases[-1]
         ])
+
+    def __repr__(self) -> str:
+        return self.specs.__str__()
