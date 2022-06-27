916a15e0a08c713ed24005ec7fae3c57d312eebb
Peng Zheng <zhengp@uw.edu>
2022-06-26 22:40:20 -0700

    Add spline integration function (#14)
    * Add the spline integration function
    
    * Add docstring

--- src/xspline/_bspline.py
+++ src/xspline/_bspline.py
@@ -3,6 +3,8 @@ from typing import Optional
 import numpy as np
 from numpy.typing import NDArray
 
+from .utils import indicator_if
+
 
 def spl_evl(t: NDArray,
             k: int,
@@ -114,6 +116,75 @@ def spl_der(t: NDArray,
         val1 = k*n1/(t[ii[3]] - t[ii[1]])
 
     val = val0 - val1
+
+    if cache is not None:
+        cache[(k, i, p)] = val
+    return val
+
+
+def spl_int(t: NDArray,
+            k: int,
+            i: int,
+            p: int,
+            x: NDArray,
+            cache: Optional[dict] = None) -> NDArray:
+    """Evaluate integrals of basis spline functions.
+
+    Parameters
+    ----------
+    t
+        Knots of the spline, assume to be non-decreasing sequence.
+    k
+        Degree of the spline, assume to be non-negative.
+    i
+        Index of the basis spline function, assume to be between `-k` and
+        `len(t) - 2`.
+    p
+        Order of integration, assume to be non-positive (we use negative number
+        to denote the order of integration to distinguish with differentiation).
+        When `p=0`, it will return the basis spline function value from
+        `spl_evl`.
+    x
+        Points where the function is evaluated.
+    cache
+        Optional cache dictionary to save computation. Default to be `None`.
+        When `cache=None`, cache will not be used in the computation.
+
+    Returns
+    -------
+    NDArray
+        Spline integral values evaluated at given points.
+
+    """
+    if (cache is not None) and ((k, i, p) in cache):
+        return cache[(k, i, p)]
+
+    if p == 0:
+        return spl_evl(t, k, i, x, cache=cache)
+
+    ii = np.maximum(np.minimum([i, i + 1, i + k, i + k + 1], t.size - 1), 0)
+    if k == 0:
+        val = np.zeros(x.shape, dtype=x.dtype)
+        if t[ii[0]] != t[ii[1]]:
+            indices = x > t[ii[0]]
+            val[indices] = indicator_if(t[ii[0]], x[indices], -p,
+                                        np.array([t[ii[0]], t[ii[1]]]))
+    else:
+        val0 = np.zeros(x.shape, dtype=x.dtype)
+        val1 = np.zeros(x.shape, dtype=x.dtype)
+
+        if t[ii[0]] != t[ii[2]]:
+            val0 = (
+                (x - t[ii[0]])*spl_int(t, k - 1, i, p, x, cache=cache) +
+                p*spl_int(t, k - 1, i, p - 1, x, cache=cache)
+            )/(t[ii[2]] - t[ii[0]])
+        if t[ii[1]] != t[ii[3]]:
+            val1 = (
+                (t[ii[3]] - x)*spl_int(t, k - 1, i + 1, p, x, cache=cache) -
+                p*spl_int(t, k - 1, i + 1, p - 1, x, cache=cache)
+            )/(t[ii[3]] - t[ii[1]])
+
+        val = val0 + val1
 
     if cache is not None:
         cache[(k, i, p)] = val
