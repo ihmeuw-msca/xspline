7ba2c59923658552265849cc92ad2d70110d095b
zhengp0 <zhengp@uw.edu>
2022-09-28 16:00:55 -0700

    add convenient import

--- src/xspline/__init__.py
+++ src/xspline/__init__.py
@@ -1,10 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-    xspline
-    ~~~~~~~
+__all__ = [
+    "Bspl",
+    "Indi",
+    "Poly",
+    "XSpline",
+    "BasisXFunction",
+    "BundleXFunction",
+    "XFunction",
+]
 
-    xspline package
-"""
 from .bspl import Bspl
 from .indi import Indi
 from .poly import Poly
+from .xfunction import BasisXFunction, BundleXFunction, XFunction
+from .xspl import XSpline
